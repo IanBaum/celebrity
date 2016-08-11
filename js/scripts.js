40af0d3ad79c22885bb228b6431cab9c8f59f219
Ian Baum and Meredith Alcorn <ian@>
2016-08-11 14:01:37 -0400

    add branching to select users match

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,58 @@ $(document).ready(function(){
     event.preventDefault();
   var age = parseInt($("#userAge").val());
   var gender = $("input:radio[name=gender]:checked").val();
-  var food = $("userFood").val();
-  });
+  var food = parseInt($("#userFood").val());
 
+  if(age) {
+
+    $("#ageDiv").removeClass("has-error")
+
+  if(food === 1) {
+    $("#audreyH").show();
+    $("#bobR").hide();
+    $("#willS").hide();
+    $("#leoD").hide();
+    $("#tinaF").hide();
+  }
+  else if(food === 2) {
+    $("#audreyH").hide();
+    $("#bobR").hide();
+    $("#willS").hide();
+    $("#leoD").show();
+    $("#tinaF").hide();
+  }
+  else if(food === 3) {
+    $("#audreyH").hide();
+    $("#bobR").show();
+    $("#willS").hide();
+    $("#leoD").hide();
+    $("#tinaF").hide();
+  }
+  else if(food === 4) {
+    $("#audreyH").hide();
+    $("#bobR").hide();
+    $("#willS").show();
+    $("#leoD").hide();
+    $("#tinaF").hide();
+  }
+  else {
+    $("#audreyH").hide();
+    $("#bobR").hide();
+    $("#willS").hide();
+    $("#leoD").hide();
+    $("#tinaF").show();
+  }
+}else{
+  $("#ageDiv").addClass("has-error");
+  $("#audreyH").hide();
+  $("#bobR").hide();
+  $("#willS").hide();
+  $("#leoD").hide();
+  $("#tinaF").hide();
+}
+
+
+
+  });
 
 });
